ebb0bdf451166862b4590fa5045c8a178a50296b
Elroy Antao <elroyantao@gmail.com>
2016-06-21 21:17:50 +0530

    delete account

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,7 +1,16 @@
+import _ from 'underscore';
+
 class HomeController {
-  constructor() {
+  constructor(Account) {
+    'ngInject';
+    this.Account = Account;
     this.name = 'home';
     console.log(this.accounts);
+  }
+  removeAccount(account){
+    this.Account.delete({id:account.id},{},()=>{
+      this.accounts = _.without(this.accounts,account);
+    });
   }
 }
 
--- client/app/components/home/home.spec.js
+++ client/app/components/home/home.spec.js
@@ -1,5 +1,9 @@
+// import angular from 'angular';
 import HomeModule from './home'
 import HomeController from './home.controller';
+import AccountResource from '../../common/resources/accounts';
+
+import Common from '../../common/common';
 
 describe('Home', () => {
   let $rootScope, makeController;
@@ -16,6 +20,15 @@ describe('Home', () => {
   });
 
   describe('Controller', () => {
+    it('removeAccount should remove an account',()=>{
+      let controller = makeController();
+
+      let accounts = [{id:1},{id:2},{id:3}];
+      controller.accounts = accounts;
+      // controller.accounts.length.should.equal(3);
+      controller.removeAccount(accounts[1]);
+      controller.accounts.lenth.should.equal(2);
+    })
   });
 
   describe('Template', () => {
